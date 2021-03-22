567e3ee2306ee66b0c0fd160a7d4dfac45c2b0a6
JuliWolf <for.julia96@gmail.com>
2021-03-22 21:48:56 +0300

    fix

--- 2. Project/pages/index.vue
+++ 2. Project/pages/index.vue
@@ -3,79 +3,19 @@
     <section class="intro">
       <h1>Get the latest tech news!</h1>
     </section>
-    <section class="featured-posts">
-      <PostPreview
-        id="1"
-        thumbnail="https://i1.wp.com/www.cloudsecuretech.com/wp-content/uploads/2017/03/Depositphotos_138324842_original.jpg?fit=1500%2C1001&ssl=1"
-        title="Hello there!"
-        previewText="This is my fist post!"
-      />
-      <PostPreview
-        id="2"
-        thumbnail="https://i1.wp.com/www.cloudsecuretech.com/wp-content/uploads/2017/03/Depositphotos_138324842_original.jpg?fit=1500%2C1001&ssl=1"
-        title="Hello there second time!"
-        previewText="This is my second post!"
-      />
-
-      <PostPreview
-        id="1"
-        thumbnail="https://i1.wp.com/www.cloudsecuretech.com/wp-content/uploads/2017/03/Depositphotos_138324842_original.jpg?fit=1500%2C1001&ssl=1"
-        title="Hi there!"
-        previewText="This is my third post!"
-      />
-    </section>
+    <PostList/>
   </div>
 </template>
 
 <script>
-import PostPreview from '@/components/Posts/PostPreview'
+import PostList from '@/components/Posts/PostList'
+
 export default {
   components: {
-    PostPreview
+    PostList
   }
 }
 </script>
 
 <style scoped>
-  .intro {
-    height: 300px;
-    position: relative;
-    padding: 30px;
-    box-sizing: border-box;
-    background-position: center;
-    background-size: cover;
-    background-image: url('~assets/images/main-page-background.jpg');
-  }
-
-  .intro h1 {
-    position: absolute;
-    top: 10%;
-    left: 5%;
-    width: 90%;
-    font-size: 1.5rem;
-    color: black;
-    background-color: rgb(211, 211, 211);
-    padding: 10px;
-    border-radius: 10px;
-    box-shadow: 3px 3px 3px black;
-    box-sizing: border-box;
-    border: 1px solid black;
-  }
-
-  @media (min-width: 768px) {
-    .intro h1 {
-      font-size: 2rem;
-    }
-  }
-
-  .featured-posts {
-    display: flex;
-    padding: 20px;
-    box-sizing: border-box;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
-  }
-
-
 </style>
