11f62056cb6a025df028011bc2124cd0d16e885b
JuliWolf <for.julia96@gmail.com>
2021-03-22 21:37:09 +0300

    reusable components

---
+++ 2. Project/layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav"/>
+    <TheSidenav
+    :show="displaySidenav"
+    @close="displaySidenav=false"
+    />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/Navigation/TheHeader';
+import TheSidenav from '@/components/Navigation/TheSidenav';
+
+export default {
+  data(){
+    return {
+      displaySidenav: false
+    }
+  },
+  components: {
+    TheSidenav,
+    TheHeader
+  },
+}
+</script>
+
+<style>
+  html{
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  body{
+    margin: 0;
+  }
+</style>
