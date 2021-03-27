e60d7af853ed330a62e76d7df38aef6379c2e8fb
JuliWolf <for.julia96@gmail.com>
2021-03-27 23:03:55 +0300

    asyncData, fetch and nuxtServerInit methods for fetching data

---
+++ 3. Handling Data/store/index.js
@@ -0,0 +1,54 @@
+import Vuex from 'vuex';
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+      loadedPosts: []
+    },
+    mutations: {
+      SET_POSTS(state, posts) {
+        state.loadedPosts = posts;
+      }
+    },
+    actions: {
+      // Executed by nuxt once
+      nuxtServerInit(vuexContext, context) {
+        if(context.store.state.loadedPosts.length > 0){
+          return null;
+        }
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            vuexContext.commit('SET_POSTS', [
+              {
+                id: '1',
+                title: "First Post",
+                previewText: "This is our first post!",
+                thumbnail:
+                  "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+              },
+              {
+                id: '2',
+                title: "Second Post",
+                previewText: "This is our second post!",
+                thumbnail:
+                  "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+              }
+            ]);
+            resolve()
+            // reject(new Error())
+          }, 1000);
+        })
+      },
+      setPosts(vuexContext, posts) {
+        vuexContext.commit('SET_POSTS', posts)
+      }
+    },
+    getters: {
+      loadedPosts(state) {
+        return state.loadedPosts;
+      }
+    }
+  })
+}
+
+export default createStore;
